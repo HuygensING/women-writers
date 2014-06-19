6b6eaf16dba49971c2062368ba6df23d56888c14
ndrsn@github.com <jona@ndrsn.org>
2014-06-19 12:12:19 +0200

    Added acceptance tests for viewing documents and persons

--- tests/acceptance/smoke/documents.js
+++ tests/acceptance/smoke/documents.js
@@ -1,16 +1,61 @@
+var defaultDelay = 5000;
+var documentSelector = '#view .document.view';
+
+function field (f) {
+	return documentSelector + ' .field-' + f;
+}
+
+function fieldValue (f) {
+	return field(f) + ' .value';
+}
+
 module.exports = {
 	"Documents" : function (browser) {
 		browser
 			.url('http://localhost:9000')
-			.waitForElementVisible('body', 5000)
-			.waitForElementVisible('.navigation', 5000)
+			.waitForElementVisible('body', defaultDelay)
+			.waitForElementVisible('.navigation', defaultDelay)
 			.click('.navigation a.document')
 			.verify.cssClassPresent('.navigation a.document', 'active')
-			.waitForElementVisible('#search .documents .results h4', 5000)
+			.waitForElementVisible('#search .documents .results h4', defaultDelay)
 			.verify.containsText('#search .documents .results h4', 'documents found')
 			.click('#search .documents .results li:first-child a')
 			.waitForElementVisible('#view .document.view h2', 2000)
 			.verify.containsText('#view .document.view h2', 'FRANCES BURNEY JOURNALS')
 			.end();
 	},
+	"Document view page": function (browser) {
+		browser
+			.url('http://localhost:9000')
+			.waitForElementVisible('body', defaultDelay)
+			.waitForElementVisible('.navigation', defaultDelay)
+			.click('.navigation a.document')
+			.verify.cssClassPresent('.navigation a.document', 'active')
+			.waitForElementVisible('#search .search-placeholder input[type=text]', defaultDelay)
+			.click('#search .documents .facet h3[data-name=dynamic_s_creator] + i')
+			.waitForElementVisible('#search .documents .facet h3[data-name=dynamic_s_creator] + i + .options input[type=text]', 2000)
+			.setValue('#search .documents .facet h3[data-name=dynamic_s_creator] + i + .options input[type=text]', 'austen')
+			.pause(1000)
+			.click('#search .documents .facet .body ul li:only-child label') // Works because the only facet with a single child is the one we just filtered on
+			.waitForElementNotVisible('#search .documents .search .overlay', defaultDelay)
+			.setValue('#search .documents .search-placeholder input[type=text]', 'Mansfield')
+			.click('#search .documents .facet.search button.search')
+			.waitForElementNotVisible('#search .documents .search .overlay', defaultDelay)
+			.waitForElementVisible('#search .documents .results h4', defaultDelay)
+			.pause(10000)
+			.verify.containsText('#search .documents .results h4', '1 document found')
+			.click('#search .documents .results li:first-child a')
+			.waitForElementVisible('#view .document.view h2', defaultDelay)
+			.verify.containsText(fieldValue('title'), 'Mansfield Park')
+			.verify.containsText(fieldValue('is-created-by'), 'Jane Austen')
+			.verify.containsText(fieldValue('has-work-language'), 'English')
+			.verify.containsText(fieldValue('has-publish-location'), 'England')
+			.verify.containsText(fieldValue('is-published-by'), 'T. Egerton')
+			.verify.containsText(fieldValue('is-stored-at'), 'British Library UK')
+			.verify.containsText(fieldValue('has-genre'), 'Novel')
+			.verify.containsText(fieldValue('edition'), '1')
+			.verify.containsText(fieldValue('date'), '1814')
+			.verify.containsText(fieldValue('notes'), 'J. Murray')
+			.end();
+	}
 }
--- tests/acceptance/smoke/persons.js
+++ tests/acceptance/smoke/persons.js
@@ -1,16 +1,65 @@
+var defaultDelay = 5000;
+var personSelector = '#view .person.view';
+
+function field (f) {
+	return personSelector + ' .field-' + f;
+}
+
+function fieldValue (f) {
+	return field(f) + ' .value';
+}
+
 module.exports = {
-	"Persons" : function (browser) {
+	"Person searching" : function (browser) {
 		browser
 			.url('http://localhost:9000')
-			.waitForElementVisible('body', 5000)
-			.waitForElementVisible('.navigation', 5000)
+			.waitForElementVisible('body', defaultDelay)
+			.waitForElementVisible('.navigation', defaultDelay)
 			.click('.navigation a.person')
 			.verify.cssClassPresent('.navigation a.person', 'active')
-			.waitForElementVisible('#search .persons .results h4', 5000)
+			.waitForElementVisible('#search .persons .results h4', defaultDelay)
 			.verify.containsText('#search .persons .results h4', 'persons found')
 			.click('#search .persons .results li:first-child a')
-			.waitForElementVisible('#view .person.view h2', 1000)
+			.waitForElementVisible('#view .person.view h2', defaultDelay)
 			.verify.containsText('#view .person.view h2', 'PAVLOVA, ANNA MIKHAILOVNA')
 			.end();
 	},
+	"Person view page": function (browser) {	
+		browser
+			.url('http://localhost:9000')
+			.waitForElementVisible('body', defaultDelay)
+			.waitForElementVisible('.navigation', defaultDelay)
+			.click('.navigation a.person')
+			.verify.cssClassPresent('.navigation a.person', 'active')
+			.waitForElementVisible('#search .search-placeholder input[type=text]', defaultDelay)
+			.setValue('#search .persons .search-placeholder input[type=text]', 'austen')
+			.click('.facet.search button.search')
+			.waitForElementNotVisible('#search .persons .search .overlay', defaultDelay)
+			.waitForElementVisible('#search .persons .results h4', defaultDelay)
+			.verify.containsText('#search .persons .results h4', '1 person found')
+			.click('#search .persons .results li:first-child a')
+			.waitForElementVisible('#view .person.view h2', 1000)
+			.verify.containsText(fieldValue('gender'), 'FEMALE')
+			.verify.containsText(fieldValue('birth-date'), '1775')
+			.verify.containsText(fieldValue('has-birth-place'), 'England')
+			.verify.containsText(fieldValue('death-date'), '1817')
+			.verify.containsText(fieldValue('nationality'), 'English')
+			.verify.containsText(fieldValue('has-person-language'), 'English')
+			.verify.containsText(fieldValue('has-marital-status'), 'Single')
+			.verify.containsText(fieldValue('has-profession'), 'Embroiderer')
+			.verify.containsText(fieldValue('has-profession'), 'Writer')
+			.verify.containsText(fieldValue('has-religion'), 'Protestant')
+			.verify.containsText(fieldValue('has-social-class'), 'Middle class')
+			.verify.containsText(fieldValue('children'), 'UNKNOWN')
+							// Don't check for all works, but just first, middle, last
+			.verify.containsText(fieldValue('is-creator-of'), 'Persuasion (1818)') // First
+			.verify.containsText(fieldValue('is-creator-of'), 'Northanger Abbey (1817)') // Middle
+			.verify.containsText(fieldValue('is-creator-of'), 'Emma (1814)') // Last
+			.verify.containsText(fieldValue('notes'), 'site Ellen Moody')
+			.verify.containsText(fieldValue('links'), 'On Jane Austen')
+			.verify.containsText(fieldValue('links'), 'Biography + weblinks')
+			.verify.containsText(fieldValue('bibliography'), 'MENTIONED IN')
+			.verify.containsText(fieldValue('personal-situation'), 'Hampshire')
+			.end();
+	}
 }
