5e7cd09e22ff2d4fbef23a802e44e0298226b230
renevanderark <r.van.der.ark@gmail.com>
2015-11-11 09:16:01 +0100

    TIM-575 show collective record

--- src/components/collective/index.jsx
+++ src/components/collective/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Link from "../link";
 import PaginationLinks from "../links/pagination";
-
 import cx from "classnames";
+
 
 class CollectiveController extends React.Component {
 
@@ -16,6 +16,16 @@ class CollectiveController extends React.Component {
 				onNavigateNextPage={this.props.onNavigateNextPage}
 				results={this.props.results} /> :
 			null;
+
+		let links = this.props.collective.links.length ?
+			this.props.collective.links.map((l, i) => <li key={i}><a className="link" href={l.url} target="_blank">{l.label}</a></li>) :
+			"-";
+
+		let documents = this.props.collective["@relations"].isStorageOf ?
+			this.props.collective["@relations"].isStorageOf.map((r, i) => (
+				<li key={i}><Link href={"documents/" + r.id} onNavigate={this.props.onNavigate} value={r.displayName} /></li>
+			)) : "-";
+
 		return (
 			<div
 				className={cx(
@@ -26,6 +36,30 @@ class CollectiveController extends React.Component {
 				{resultsLink}
 				<header className="page"><h2>{this.props.collective.name}</h2></header>
 				{paginationLinks}
+				<div className="record-container">
+					<ul className="record">
+						<li>
+							<label>Name</label>
+							{this.props.collective.name}
+						</li>
+						<li>
+							<label>Type</label>
+							{this.props.collective.type}
+						</li>
+						<li>
+							<label>Has location</label>
+							{(this.props.collective["@relations"].hasLocation || []).map((r) => r.displayName).join(", ")}
+						</li>
+						<li>
+							<label>Links</label>
+							<ul className="relation">{links}</ul>
+						</li>
+						<li>
+							<label>Is storage of</label>
+							<ul className="relation">{documents}</ul>
+						</li>
+					</ul>
+				</div>
 			</div>
 		);
 	}
